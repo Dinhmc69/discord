15caefc8690cf409540d48fa9b32b17a0a965089
Dinhmc69 <nqnlts0817291@outlook.com>
2025-07-27 07:22:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 print("HI")
+import requests
+msg = "Auto send from auto download and run code"
+webhook_url = "https://discord.com/api/webhooks/1376571321168691322/b1NBoB9TTySn-b4R-r6e_5lcvDqsVNSBfPQMgNoBJNXeIjjFl7reUOXX1U-6TT0vINgr"
+payload = {"content": msg}
+requests.post(webhook_url, data=payload, timeout=30)
